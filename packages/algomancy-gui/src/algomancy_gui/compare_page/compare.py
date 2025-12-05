6a7467939fef8bbfa194c9a63f14b24b84897e45
Pepijn Wissing <Wsg@cqm.nl>
2025-12-05 10:39:08 +0100

    moved compare callbacks to compare.py

--- packages/algomancy-gui/src/algomancy_gui/compare_page/compare.py
+++ packages/algomancy-gui/src/algomancy_gui/compare_page/compare.py
@@ -10,6 +10,7 @@ from typing import Any
 from dash import html, get_app, callback, Output, Input, State
 import dash_bootstrap_components as dbc
 
+from .kpicard import kpi_card
 from ..componentids import (
     PERF_DETAILS_COLLAPSE,
     COMPARE_DETAIL_VIEW,
@@ -25,6 +26,8 @@ from ..componentids import (
     RIGHT_SCENARIO_OVERVIEW,
     RIGHT_SCENARIO_DROPDOWN,
     COMPARE_PAGE,
+    PERF_SBS_RIGHT_COLLAPSE,
+    PERF_SBS_LEFT_COLLAPSE,
 )
 from ..compare_page.scenarioselector import (
     create_side_by_side_viewer,
@@ -302,3 +305,62 @@ def update_right_scenario_overview_detail(
         return "One of the scenarios was not found."
 
     return cr.compare_details(left_scenario, right_scenario)
+
+
+@callback(
+    Output(KPI_IMPROVEMENT_SECTION, "children"),
+    Input(LEFT_SCENARIO_DROPDOWN, "value"),
+    Input(RIGHT_SCENARIO_DROPDOWN, "value"),
+)
+def update_kpi_comparison(left_id, right_id):
+    if not left_id or not right_id:
+        return html.P("Select two completed scenarios to compare KPIs.")
+    sm: ScenarioManager = get_app().server.scenario_manager
+
+    left = sm.get_by_id(left_id)
+    right = sm.get_by_id(right_id)
+
+    if not left or not right:
+        return html.P("One or both scenarios not found.")
+
+    # Example KPI dictionaries
+    left_kpis = left.kpis
+    right_kpis = right.kpis
+
+    assert len(left_kpis) == len(right_kpis), "KPIs do not match."
+
+    cards = []
+    for tag, left_kpi in left_kpis.items():
+        right_kpi = right_kpis.get(tag)
+
+        card = kpi_card(
+            left_kpi=left_kpi,
+            right_kpi=right_kpi,
+        )
+
+        cards.append(html.Div(card, className="kpi-card-wrapper"))
+
+    return html.Div(cards, className="kpi-cards")
+
+
+@callback(
+    Output(PERF_SBS_LEFT_COLLAPSE, "is_open"),
+    Output(PERF_SBS_RIGHT_COLLAPSE, "is_open"),
+    Output(PERF_KPI_COLLAPSE, "is_open"),
+    Input(PERF_TOGGLE_CHECKLIST_LEFT, "value"),
+)
+def listen_to_left_checklist(checked):
+    sbs_open = True if "side-by-side" in checked else False
+    kpi_open = True if "kpis" in checked else False
+    return sbs_open, sbs_open, kpi_open
+
+
+@callback(
+    Output(PERF_COMPARE_COLLAPSE, "is_open"),
+    Output(PERF_DETAILS_COLLAPSE, "is_open"),
+    Input(PERF_TOGGLE_CHECKLIST_RIGHT, "value"),
+)
+def listen_to_right_checklist(checked):
+    compare_open = True if "compare" in checked else False
+    details_open = True if "details" in checked else False
+    return compare_open, details_open
