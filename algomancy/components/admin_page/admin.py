e7fac6d256bde3984b4ae48fbb20f72094a5f211
hgd-cqm <rens.hoogendorp@cqm.nl>
2025-12-09 15:53:53 +0100

    add admin page callbacks

--- algomancy/components/admin_page/admin.py
+++ algomancy/components/admin_page/admin.py
@@ -3,6 +3,7 @@ from dash import html
 
 from algomancy.components.componentids import ADMIN_PAGE
 
+import algomancy.components.admin_page.callbacks
 
 def admin_page():
     return html.Div(id=ADMIN_PAGE)
