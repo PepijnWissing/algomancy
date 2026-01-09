a7ba5af4c817592d07b21ba9ed147daaf5964d63
Pepijn Wissing <Wsg@cqm.nl>
2026-01-09 15:27:39 +0100

    Update placeholderetlfactory.py

--- packages/algomancy-content/src/algomancy_content/backend/placeholderetlfactory.py
+++ packages/algomancy-content/src/algomancy_content/backend/placeholderetlfactory.py
@@ -1,52 +1,26 @@
-from typing import Dict, cast
+from typing import Dict
 
 import algomancy_data as de
+from algomancy_data import File
+from algomancy_data.extractor import ExtractionSequence
+from algomancy_data.transformer import TransformationSequence
 
 
 class PlaceholderETLFactory(de.ETLFactory):
     def __init__(self, configs, logger=None):
         super().__init__(configs, logger)
 
-    def create_extractors(self, files: Dict[str, de.File]) -> Dict[str, de.Extractor]:
-        # define file name(s) for convenient access
-        placeholder = "placeholder_data"
+    def create_extraction_sequence(self, files: Dict[str, File]) -> ExtractionSequence:
+        # return the empty sequence
+        return ExtractionSequence(logger=self.logger)
 
-        # compile a convenient mapping of file names to schemas
-        schemas = {cfg.file_name: cfg.file_schema for cfg in self.input_configurations}
-
-        # create an extractor for each file
-        extractors = {
-            placeholder: de.CSVSingleExtractor(
-                file=cast(de.CSVFile, files[placeholder]),
-                schema=schemas[placeholder],
-                logger=self.logger,
-                separator=",",
-            )
-        }
-
-        # output the extractors
-        return extractors
+    def create_transformation_sequence(self) -> TransformationSequence:
+        # return the empty sequence
+        return TransformationSequence(logger=self.logger)
 
     def create_validation_sequence(self) -> de.ValidationSequence:
-        # construct the empty sequence
-        vs = de.ValidationSequence(logger=self.logger)
-
-        # add a validator to check for successful extraction
-        vs.add_validator(de.ExtractionSuccessVerification())
-
-        # add a validator to check datatypes of the extracted data
-        vs.add_validator(
-            de.InputConfigurationValidator(
-                configs=self.input_configurations,
-                severity=de.ValidationSeverity.CRITICAL,
-            )
-        )
-
-        # output the sequence
-        return vs
-
-    def create_transformers(self) -> Dict[str, de.Transformer]:
-        return {}
+        # return the empty sequence
+        return de.ValidationSequence(logger=self.logger)
 
     def create_loader(self) -> de.Loader:
         return de.DataSourceLoader(self.logger)
