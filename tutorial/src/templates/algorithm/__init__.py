ae7f00f5d3b4641634710e8a53dcf60128d4f435
Pepijn Wissing <Wsg@cqm.nl>
2026-02-09 11:57:56 +0100

    Added tutorial algorithms

---
+++ tutorial/src/templates/algorithm/__init__.py
@@ -0,0 +1,7 @@
+from templates.algorithm.nearest_neighbor import NearestNeighborAlgorithm
+from templates.algorithm.simulated_annealing import SimulatedAnnealingAlgorithm
+
+algorithm_templates = {
+    "NearestNeighbor": NearestNeighborAlgorithm,
+    "SimulatingAnnealing": SimulatedAnnealingAlgorithm,
+}
