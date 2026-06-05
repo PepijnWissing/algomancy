85c491fced5d1831ef7b7036b13cd4d700870511
Pepijn Wissing <Wsg@cqm.nl>
2026-06-05 08:38:30 +0200

    added persistent scenario sql option

--- packages/algomancy-scenario/src/algomancy_scenario/persistence/__init__.py
+++ packages/algomancy-scenario/src/algomancy_scenario/persistence/__init__.py
@@ -1,7 +1,9 @@
+from .protocols import SqlResultLayout
 from .repository import ScenarioRepository
 from .sql_repository import SqlScenarioRepository
 
 __all__ = [
     "ScenarioRepository",
+    "SqlResultLayout",
     "SqlScenarioRepository",
 ]
